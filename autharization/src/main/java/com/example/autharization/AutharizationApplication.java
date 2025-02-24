816a5762441e00f0dbce40ec2441c690aafbe0e4
arioshaman1 <adaikin.andr321@gmail.com>
2025-02-24 05:25:11 +0300

    Your commit message

---
+++ autharization/src/main/java/com/example/autharization/AutharizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.autharization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutharizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutharizationApplication.class, args);
+	}
+
+}
